2db337ac67146d6a0e8ec8a13f0ec49a309052df
Marissa Rodenburg <rodenm@gmail.com>
2024-11-15 16:27:48 -0500

    Second commit

--- my_server.py
+++ my_server.py
@@ -29,3 +29,8 @@ def not_found(e):
 
 if __name__ == '__main__': 
     app.run(port=8000, debug=True)
+
+# Here are the git instructions to create a repository
+# git init
+# git add .
+# git commit -m "First commit"
